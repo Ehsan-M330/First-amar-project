fac42358099fdda521225ed0379feecb8af4ca31
mofaraz <mopharaz@gmail.com>
2022-12-07 14:54:13 +0330

    create a model fon continuous data

---
+++ ContinuousData.py
@@ -0,0 +1,14 @@
+class Data:
+
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+        self.xi = ((second - first) / 2) + first
+        self.ri = 0
+        self.fi = 0
+        self.gi = 0
+        self.si = 0
+
+
+
+
